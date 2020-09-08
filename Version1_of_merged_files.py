4c04166265985b04d67eaeb05a8ba8daf16891e0
Dragana1404 <draganagagajocic@gmail.com>
2020-09-08 14:50:31 +0200

    Version 1

---
+++ Version1_of_merged_files.py
@@ -0,0 +1,24 @@
+#Merge CSV files from different folders
+import os
+import pandas as pd
+from glob import glob
+
+path = "C:/Users/Nenad/Desktop/Data"
+EXT = "data1.csv"
+all_csv_files = [file
+                 for path, subdir, files in os.walk(path)
+                 for file in glob(os.path.join(path, EXT))]
+
+for f in all_csv_files:
+    df=pd.read_csv(f, header=None)
+    print("The number of rows: ",df.shape[0])
+print('\n')
+combined_csv_data =pd.concat([pd.read_csv(f, header=None)for f in all_csv_files])
+combined_csv_data.to_csv('combined_data.csv')
+print("Combined CSV file: ")
+print(combined_csv_data)
+print("The number of rows is: ",combined_csv_data.shape[0])
+print('\n')
+exe='combined_data.csv'
+print("The path from new file is: ")
+print(os.path.abspath(exe))
